34796b8407b141babccbcee8923a1b2b8e993791
Chris <iceman7376@yahoo.com>
2018-09-15 17:27:05 -0400

    Add DHT

--- GarageDoorSensor/auth-template.h
+++ GarageDoorSensor/auth-template.h
@@ -37,7 +37,7 @@
 #define DOOR1_ALIAS "Door 1"
 #define MQTT_DOOR1_ACTION_TOPIC WIFI_HOSTNAME "/1/action"
 #define MQTT_DOOR1_STATUS_TOPIC WIFI_HOSTNAME "/1/status"
-#define DOOR1_RELAY_PIN 10 // SD3 on ESP8266
+#define DOOR1_RELAY_PIN 4 // D2 on ESP8266
 #define DOOR1_ECHO_PIN 14 // D5 on ESP8266
 
 // Door 2 Parameters
@@ -45,7 +45,7 @@
 #define DOOR2_ALIAS "Door 2"
 #define MQTT_DOOR2_ACTION_TOPIC WIFI_HOSTNAME "/2/action"
 #define MQTT_DOOR2_STATUS_TOPIC WIFI_HOSTNAME "/2/status"
-#define DOOR2_RELAY_PIN 4 // D2 on ESP8266
+#define DOOR2_RELAY_PIN 0 // D3
 #define DOOR2_ECHO_PIN 12 // D6 on ESP8266
 
 // Door 3 Parameters
@@ -54,6 +54,6 @@
 #define MQTT_DOOR3_ACTION_TOPIC WIFI_HOSTNAME "/3/action"
 #define MQTT_DOOR3_STATUS_TOPIC WIFI_HOSTNAME "/3/status"
 #define DOOR3_RELAY_PIN 5 // D1 on ESP8266
-#define DOOR3_ECHO_PIN 13 // D7 on ESP8266
+#define DOOR3_ECHO_PIN 10 // SD3 on ESP8266
 
 #endif
